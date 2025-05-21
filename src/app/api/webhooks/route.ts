7a4b18d7d64f18d4628987e7398a121711fc55bf
Arun Biradar <aruix9@gmail.com>
2025-05-21 07:58:01 +0530

    fix secret key

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
     const signature = req.headers.get('x-razorpay-signature')
 
     const expectedSignature = crypto
-      .createHmac('sha256', process.env.RAZORPAY_SECRET!)
+      .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET!)
       .update(body)
       .digest('hex')
 
