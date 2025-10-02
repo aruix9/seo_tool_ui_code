4216b0363fab3b1740263af0c8628e230619cb5e
Arun B <aruix9@gmail.com>
2025-10-03 03:02:07 +0530

    fix audit actions build issue

--- src/lib/actions/audit/auditActions.ts
+++ src/lib/actions/audit/auditActions.ts
@@ -1,30 +1,32 @@
 import axios from 'axios'
 // import {backlinks, backlinks2} from "../../../../auditData"
 
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+
 export async function getSummary(targets: string[]) {
   // return backlinks2;
   const target = {target: targets}
-  const response = await axios.post('http://localhost:3000/api/v1/audit/summary', target)
+  const response = await axios.post(API_BASE + '/api/v1/audit/summary', target)
   return response.data.summary
 }
 
 export async function getMetrics(targets: string[]) {
   // return backlinks2;
   const target = {target: targets}
-  const response = await axios.post('http://localhost:3000/api/v1/audit/metrics', target)
+  const response = await axios.post(API_BASE + '/api/v1/audit/metrics', target)
   return response.data.metrics
 }
 
 export async function getBacklinksMetrics(targets: string[]) {
   // return backlinks2;
   const target = {target: targets}
-  const response = await axios.post('http://localhost:3000/api/v1/audit/backlinks-metrics', target)
+  const response = await axios.post(API_BASE + '/api/v1/audit/backlinks-metrics', target)
   return response.data.metrics
 }
 
 export async function getHistory(target: string, fromDate: string, toDate: string) {
   // return backlinks2;
   const body = {target, fromDate, toDate}
-  const response = await axios.post('http://localhost:3000/api/v1/audit/history', body)
+  const response = await axios.post(API_BASE + '/api/v1/audit/history', body)
   return response.data.new_lost_backlinks
 }
