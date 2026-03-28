b3c53b4bc5a3e202cdf926fdb4a263110be2b865
Arun B <aruix9@gmail.com>
2026-03-29 04:52:54 +0530

    Remove recently audit sections

--- src/app/(tool)/audit/(home)/index.tsx
+++ src/app/(tool)/audit/(home)/index.tsx
@@ -7,27 +7,27 @@ import AuditForm from "./AuditForm";
 import HowItWorks from "./cms/HowItWorks";
 
 const AuditHomePage = () => {
-    return (
-        <>
-            <section className="border-b border-slate-200 bg-white/50">
-                <div className="max-w-[1440px] px-6 mx-auto mb-12">
-                    <div className="grid grid-cols-1 lg:grid-cols-10 gap-8">
-                        <div className="lg:col-span-6 bg-white rounded-xl p-8 md:p-10 shadow-sm border border-slate-200">
-                            <HeroTitle />
-                            <AuditForm />
-                        </div>
+  return (
+    <>
+      <section className="border-b border-slate-200 bg-white/50">
+        <div className="max-w-[1440px] px-6 mx-auto mb-12">
+          <div className="grid grid-cols-1 lg:grid-cols-10 gap-8">
+            <div className="lg:col-span-6 bg-white rounded-xl p-8 md:p-10 shadow-sm border border-slate-200">
+              <HeroTitle />
+              <AuditForm />
+            </div>
 
-                        <aside className="lg:col-span-4 h-full flex flex-col">
-                            <AuthorityRoadmap currentStep={1} />
-                            <AnalysisTips />
-                        </aside>
-                    </div>
-                </div>
-            </section>
-            <RecentlyAnalyzed />
-            <HowItWorks />
-        </>
-    )
-}
+            <aside className="lg:col-span-4 h-full flex flex-col">
+              <AuthorityRoadmap currentStep={1} />
+              <AnalysisTips />
+            </aside>
+          </div>
+        </div>
+      </section>
+      {/* <RecentlyAnalyzed /> */}
+      <HowItWorks />
+    </>
+  );
+};
 
-export default AuditHomePage
+export default AuditHomePage;
